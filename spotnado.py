cef05984632dec54eef48f1262a9fe21fc6f6fa1
Matt Jones <mkjones@cs.stanford.edu>
2011-10-17 01:24:32 -0700

    add slightly less ghetto volume meter

--- spotnado.py
+++ spotnado.py
@@ -6,24 +6,28 @@ import subprocess
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         s = Spotify()
-        verbs = ['skip', 'louder', 'quieter']
+        verbs = ['skip']
         if s.isPlaying():
             verbs.append('pause')
         else:
             verbs.append('play')
 
+        volume_filled = int(s.getVolume()/10)
+
         self.render('index.html',
                     verbs=verbs,
                     track_name=s.getTrackName(),
-                    track_artist=s.getTrackArtist())
+                    track_artist=s.getTrackArtist(),
+                    volume_filled = volume_filled,
+                    volume_empty = 10 - volume_filled)
 
 class DoHandler(tornado.web.RequestHandler):
     def post(self):
         verb = self.get_argument('verb')
         s = Spotify()
-        if verb== 'louder':
+        if verb == '+':
             s.louder()
-        elif verb == 'quieter':
+        elif verb == '-':
             s.quieter()
         else:
             s.tell(verb)
